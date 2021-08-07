b8eacf4a2695ebf4cad9aadf37ae12309b98b93b
SWATI PANDEY <swati.pandey@somaiya.edu>
2021-08-08 00:01:11 +0530

    restapi

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -19,5 +19,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snippets/',include('snippets.urls'))
+    path('snippets/',include('snippets.urls')),
 ]
